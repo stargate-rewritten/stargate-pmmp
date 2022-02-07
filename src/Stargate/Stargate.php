b6eae62890e49ac5bcd06bdd82078297e2aec0b4
Pheotis <49039536+Pheotis@users.noreply.github.com>
2022-02-07 14:15:10 -0500

    Matched file structure from stargate-hmod.

---
+++ src/Stargate/Stargate.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Description needed.
+ *
+ * @author Shaun, Dinnerbone, Pheotis
+ */
+
+declare(strict_types=1);
+
+namespace Stargate;
+
+use pocketmine\plugin\PluginBase;
+
+class Stargate extends PluginBase{
+
+}
